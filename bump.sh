42f58208bbc8c1fc63c678c6d06a3d4bd5798edb
baluyotraf <baluyotraf@outlook.com>
2025-05-06 18:01:25 +0200

    ci: add script to bump the version and changelogs properly

---
+++ bump.sh
@@ -0,0 +1,3 @@
+cz --config pyproject.toml --debug bump --yes
+cz --config repo-a/pyproject.toml --debug changelog --file-name repo-a/CHANGELOG.md
+cz --config repo-b/pyproject.toml --debug changelog --file-name repo-b/CHANGELOG.md
