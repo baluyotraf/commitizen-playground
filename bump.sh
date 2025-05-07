3906a517575a10e4b22de3510fa054bf64afcdf8
baluyotraf <baluyotraf@outlook.com>
2025-05-07 13:50:58 +0200

    ci: improve the bump script to include the template and be scalable

--- bump.sh
+++ bump.sh
@@ -1,3 +1,18 @@
+
+SCRIPT_DIR="$(dirname "$(realpath "${0}")")"
+PROJECTS=("repo-a" "repo-b")
+CHANGELOG_TEMPLATE="${SCRIPT_DIR}/CHANGELOG.md.j2"
+RELATIVE_TEMPLATE="$(realpath --relative-to="${PWD}" "${CHANGELOG_TEMPLATE}")"
+
+echo ${CHANGELOG_TEMPLATE}
+echo ${RELATIVE_TEMPLATE}
+
 cz --config pyproject.toml --debug bump --yes
-cz --config repo-a/pyproject.toml --debug changelog --file-name repo-a/CHANGELOG.md
-cz --config repo-b/pyproject.toml --debug changelog --file-name repo-b/CHANGELOG.md
+
+for proj in "${PROJECTS[@]}"; do
+    cz --debug \
+        --config "${SCRIPT_DIR}/${proj}/pyproject.toml" \
+        changelog \
+            --template "${RELATIVE_TEMPLATE}" \
+            --file-name "${SCRIPT_DIR}/${proj}/CHANGELOG.md"
+done
